83be6a64bd6360279a25b059138ba51346decc13
teripanda <teri.london@gmail.com>
2018-05-11 15:15:33 -0700

    added moAr game logic

--- js/app.js
+++ js/app.js
@@ -10,29 +10,37 @@ console.log("Up and running!");
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0]
-var cardTwo = cards[3]
-var cardThree = cards
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You got dis here match!");
+		} else {
+			alert("No match 4 u");	
+		}
+	}
+};
 
-cardsInPlay.push('cardOne');
+var flipCard = function(cardId){
+	cardsInPlay.push(cardId);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+};
 
-console.log("User flipped " + cardOne);
+flipCard(0);
+flipCard(1);
 
 
-cardsInPlay.push('cardTwo');
-console.log("User flipped " + cardTwo);
 
-
-if (cardsInPlay.length === 2) {
 	// console.log("first if")
 	// console.log(cardsInPlay[0])
 	// console.log(cardsInPlay[1])
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You got dis here match!");
-} else {
-	alert("No match 4 u");	
-}
-};
+
+// var cardOne = cards[0]
+// var cardTwo = cards[3]
+
+// cardsInPlay.push('cardOne');
+// console.log("User flipped " + cardOne);
 
 
-
+// cardsInPlay.push('cardTwo');
+// console.log("User flipped " + cardTwo);
