b53e1969944e302454412e325442c10262321d63
teripanda <teri.london@gmail.com>
2018-05-11 14:41:01 -0700

    created game logic

--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
+
+// console.log("User flipped " + cardThree);
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var cardOne = cards[0]
+var cardTwo = cards[3]
+var cardThree = cards
+
+cardsInPlay.push('cardOne');
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+
+cardsInPlay.push('cardTwo');
+console.log("User flipped " + cardTwo);
+
+
+if (cardsInPlay.length === 2) {
+	// console.log("first if")
+	// console.log(cardsInPlay[0])
+	// console.log(cardsInPlay[1])
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You got dis here match!");
+} else {
+	alert("No match 4 u");	
+}
+};
+
+
+
